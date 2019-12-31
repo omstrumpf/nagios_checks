03cce945164eafaa27bcb5b65171b52f35e1d863
omstrumpf <omstrumpf@gmail.com>
2019-12-31 01:04:44 -0500

    checking for scrub repaired errors

--- check_zfs_errors/check_zfs_errors.py
+++ check_zfs_errors/check_zfs_errors.py
@@ -9,6 +9,9 @@ ERRORS_REGEX = re.compile(
     r"^\s*(?P<entry>\S+)\s+(?P<state>\S+)\s+(?P<read>\d+)\s+(?P<write>\d+)\s+(?P<checksum>\d+)\s+(?P<slow>\d+)$",
     re.MULTILINE,
 )
+SCRUB_REGEX = re.compile(
+    r"scan:.*repaired (?P<bytes>\S+) .* with (?P<errors>\d+) errors"
+)
 
 # Parse arguments
 parser = argparse.ArgumentParser(
@@ -27,7 +30,20 @@ stdout, stderr = subprocess.Popen(
     stderr=subprocess.PIPE,
 ).communicate()
 
-matches = re.findall(ERRORS_REGEX, stdout.decode("utf-8"))
+result = stdout.decode("utf-8")
+
+scrub_match = re.search(SCRUB_REGEX, result)
+
+scrub_warning = None
+if not scrub_match:
+    print(f"ZFS Errors WARNING ({pool_name}): No recent scrub.")
+else:
+    if int(scrub_match["errors"]) > 0:
+        repaired_errors = scrub_match["errors"]
+        repaired_bytes = scrub_match["bytes"]
+        scrub_warning = f"ZFS Errors WARNING ({pool_name}): Scrub repaired {repaired_bytes} with {repaired_errors} errors."
+
+matches = re.findall(ERRORS_REGEX, result)
 
 num_online = 0
 total_r = 0
@@ -46,13 +62,15 @@ for name, status, r, w, c, s in matches:
 total_all = total_r + total_w + total_c + total_s
 
 if total_all == 0 and num_online == len(matches):
-    # TODO check for errors repaired in scrub, warning if so.
-    print(
-        f"ZFS Errors OK ({pool_name}): All {num_online} entries online with no errors."
-    )
+    if scrub_warning:
+        print(scrub_warning)
+    else:
+        print(
+            f"ZFS Errors OK ({pool_name}): All {num_online} entries online with no errors."
+        )
 elif num_online != len(matches):
     print(
-        f"ZFS Errors Critical ({pool_name}): {len(matches) - num_online} entries not online."
+        f"ZFS Errors CRITICAL ({pool_name}): {len(matches) - num_online} entries not online."
     )
 else:
-    print(f"ZFS Errors Critical ({pool_name}): {total_all} errors detected.")
+    print(f"ZFS Errors CRITICAL ({pool_name}): {total_all} errors detected.")
